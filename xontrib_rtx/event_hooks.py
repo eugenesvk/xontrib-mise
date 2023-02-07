d4506bb5437e759cf1a21497d3769ebab2e61b8c
eugenesvk <eugenesvk@users.noreply.github.com>
2023-02-07 15:59:49 +0700

    remove standalone event hooks

--- xontrib_rtx/event_hooks.py
+++
@@ -1,9 +0,0 @@
-def activate_env(path:str):
-    # lazy import to reduce startup cost
-    from xontrib.voxapi import Vox
-
-    vox = Vox()
-    vox.activate(path)
-
-def listen_cd(olddir, newdir, **_):
-    activate_env(newdir)
