505c921f31524ae6eba2d59837611bef7d13dbdd
eugenesvk <eugenesvk@users.noreply.github.com>
2026-07-12 21:52:04 +0300

    test: add loading as a fixture

--- tests/test_xontrib.py
+++ tests/test_xontrib.py
@@ -1,2 +1,5 @@
+import pytest
+
+@pytest.fixture(autouse=True) # make all tests automatically request this fixture
 def test_it_loads(load_xontrib):
-  load_xontrib("mise")
+  return load_xontrib("mise")
